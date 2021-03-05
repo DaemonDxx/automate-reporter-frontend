9899a092bd5e9323bf00c63fadf5d893952a6a9e
DaemonDxx <i79535999238@gmail.com>
2021-03-05 20:59:05 +0700

    Добавлена обработка ошибок в API Auth

--- src/API/Auth.js
+++ src/API/Auth.js
@@ -4,18 +4,36 @@ const URL_AUTH = 'auth/login'
 const URL_REG = 'user/'
 
 export async function Registration(username, password, key) {
-    const response = await http.post(URL_REG, {
-        username,
-        password,
-        key,
-    });
-    return response.data;
+    try {
+        const response = await http.post(URL_REG, {
+            username,
+            password,
+            key,
+        });
+        if (response.status === 201) {
+            const user = await response.data;
+            return {err: null, response: user}
+        } else {
+            return {err: `Запрос завершился со статусом ${response.status}`}
+        }
+    } catch (e) {
+        return {err: `Ошибка ${e.message}`}
+    }
 }
 
 export async function Login(username, password) {
-    const response = await http.post(URL_AUTH, {
-        username,
-        password,
-    });
-    return response.data;
+    try {
+        const response = await http.post(URL_AUTH, {
+            username,
+            password,
+        });
+        if (response.status === 201) {
+            const payload = await response.data;
+            return {err: null, response: payload}
+        } else {
+            return {err: `Запрос завершился со статусом ${response.status}`}
+        }
+    } catch (e) {
+        return {err: `Ошибка ${e.message}`}
+    }
 }
