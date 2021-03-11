1de82caa11dfd2972c00476e764945b10f626c14
DaemonDxx <i79535999238@gmail.com>
2021-03-11 19:29:32 +0700

    Промежуточный коммит

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,23 +4,10 @@
           v-if="isAuth"
           color="blue darken-2"
           app>
-        <v-app-bar-nav-icon>
-          Б
+        <v-app-bar-nav-icon
+          @click="isShowNav=!isShowNav"
+        >
         </v-app-bar-nav-icon>
-        <v-btn class="ml-4 mr-4"
-               color="white"
-               outlined
-               @click="$router.push('/report')"
-        >
-          Отчеты
-        </v-btn>
-        <v-btn class="ml-4 mr-4"
-               color="white"
-               outlined
-               @click="$router.push('/offsets')"
-        >
-          Температурный фактор
-        </v-btn>
         <v-spacer>
 
         </v-spacer>
@@ -33,6 +20,36 @@
       </v-app-bar>
     <notifications
     />
+    <v-navigation-drawer
+      app
+      absolute
+      temporary
+      v-model="isShowNav"
+    >
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item-group
+          v-model="selectedRoute"
+          color="primary"
+        >
+          <v-list-item
+              v-for="item in routes.offset"
+              :key="item.title"
+              dense
+          >
+            <v-list-item-icon>
+              <v-icon>{{item.icon}}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              {{item.title}}
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+
+      </v-list>
+    </v-navigation-drawer>
     <v-main class="grey">
         <router-view>
 
@@ -48,6 +65,19 @@ import {ACTION_LOGOUT} from "@/store/auth";
 
 export default {
   name: "MainLayout",
+  data: () => {
+    return {
+      isShowNav: false,
+      routes: {
+        offset: [
+          {title: 'История', icon: 'mdi-database', path: '/offsets/history'},
+          {title: 'Расчет', icon: 'mdi-calculator', path: '/offsets/solver'},
+          {title: 'Изменение данных', icon: 'mdi-pencil-plus', path: '/offsets/changer'}
+        ]
+      },
+      selectedRoute: {}
+    }
+  },
   methods: {
     ...mapActions([ACTION_LOGOUT]),
 
--- src/layouts/TemperatureLayout.vue
+++ src/layouts/TemperatureLayout.vue
@@ -1,37 +1,6 @@
 <template>
   <v-container>
-    <v-navigation-drawer
-      permanent
-      absolute
-      mini-variant
-      expand-on-hover
-    >
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-group
-            color="primary"
-            @change="goPath"
-            v-model="currentPath"
-        >
-          <v-list-item
-              v-for="item in navItems"
-              :key="item.title"
-          >
-            <v-list-item-icon>
-              <v-icon v-text="item.icon"></v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              {{item.title}}
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-    <router-view>
 
-    </router-view>
   </v-container>
 </template>
 
